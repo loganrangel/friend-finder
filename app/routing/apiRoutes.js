2daca8a70db94b5eb4b333cd6ebfe60155087dc5
Logan Angel <you@example.com>
2019-08-21 11:03:21 -0400

    added server, get post, api

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+
+  app.get("/api/", function(req, res) {
+    res.json();
+  });
+
+  app.get("/api/", function(req, res) {
+    res.json();
+  });
+
+
+
+  app.post("/api/", function(req, res) {
+
+  });
